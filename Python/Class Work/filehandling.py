5e1265b9f6fcac2b9c500580bb1345f1fb107072
lucky01020 <tiwarilucky042@gmail.com>
2024-10-03 02:17:41 +0530

    Assignment

--- Python/Class Work/filehandling.py
+++ Python/Class Work/filehandling.py
@@ -1,3 +1,22 @@
+"""
 file = open("C:\\Users\Admin\\Documents\\c programming\\classworkhello.txt","w")
 file.write("This is my world")
 file.close()
+"""
+"""
+file = open("C:\\Users\Admin\\Documents\\c programming\\classworkhello.txt","a")
+file.write("\nThis is append method")
+file.close()
+"""
+
+"""
+file = open("C:\\Users\Admin\\Documents\\c programming\\classworkhello1.txt","w+")
+file.write("Hello this is w+ method")
+print(file.tell())
+file.seek(0)
+print(file.readline())
+file.close()
+"""
+
+
+
